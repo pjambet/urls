890decc71deeaafe51e5be4de2bfbb8ef10f01ba
pjambet <pierre.jambet@gmail.com>
2013-09-07 20:16:05 +0200

    Checkpoint

--- web.go
+++ web.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"html/template"
+	"net/http"
+	"os"
+	/* "strings" */
 )
 
+var con redis.Conn = nil
+
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/shorten/", shorten)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	fmt.Println("listening...")
+
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+	// TODO : Redirect to the URL form the request after trying to find it
+	/* fmt.Println(strings.TrimPrefix(req.URL.Path, "/")) */
+	// in Redis
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(res, nil)
+}
+
+func shorten(res http.ResponseWriter, req *http.Request) {
+
 }
